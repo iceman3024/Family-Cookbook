6dc5ddef760f201a3f48148523c67b7a2ca60e47
iceman3024 <jesusfreak3024@gmail.com>
2025-12-01 10:05:11 -0500

    Initialize main.jsx with React and App component

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // This must import App.jsx correctly!
+
+// Since we are using Tailwind classes directly, we don't need a separate CSS import.
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
